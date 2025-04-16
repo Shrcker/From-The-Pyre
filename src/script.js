6f89aa93e6d08bc156dbaa14a91ff599f4300d99
Tania <tanner.shirkey@gmail.com>
2025-04-16 13:44:26 -0400

    Added axe
    Added the axe, the first tool that will allow you to harvest wood more efficiently. Right now it doesn't cost time to create tools, as this feature may be divorced from the main day game loop at some point and be replaced by another system, potentially one where you make it overnight.

--- src/script.js
+++ src/script.js
@@ -1,15 +1,40 @@
 const timeCounter = document.getElementById("time-counter");
-const chopBtn = document.getElementById("chop-wood");
-const gatherBtn = document.getElementById("gather-stone");
+const woodBtn = document.getElementById("get-wood");
+const stoneBtn = document.getElementById("get-stone");
 const stoneCounter = document.getElementById("stone-counter");
 const woodCounter = document.getElementById("wood-counter");
+const axeBtn = document.getElementById("get-axe");
+const counterList = document.getElementById("counter-list");
+const actionList = document.getElementById("action-list");
+const messageCenter = document.getElementById("message-center");
+
+const newMessage = document.createElement("p");
 
 let timeNumber = 12;
 let woodNumber = 1;
 let stoneNumber = 1;
+let isAxeMade = false;
+let wasAxeBtnShown = false;
 
-const setupGame = () => {
-  timeCounter.innerHTML = `Time: ${timeNumber}`;
+const updateGame = () => {
+
+  if (timeCounter.innerHTML = `A New Day Begins...<br>Time: ${timeNumber}`) {
+    timeCounter.innerHTML = `Time: ${timeNumber}`;
+  }
+
+  if (woodNumber >= 6 && !wasAxeBtnShown) {
+    const newAxeBtn = document.createElement("button");
+    newAxeBtn.innerHTML = "Create Axe (8 wood, 4 stone)";
+    newAxeBtn.id = "get-axe";
+    newAxeBtn.classList.add("button");
+    newAxeBtn.addEventListener("click", makeAxe);
+
+    actionList.appendChild(newAxeBtn);
+    wasAxeBtnShown = true;
+  }
+
+  messageCenter.innerHTML = '';
+  newMessage.innerHTML = '';
 }
 
 const updateTime = (amount) => {
@@ -20,12 +45,14 @@ const updateTime = (amount) => {
     timeNumber = 12;
     timeCounter.innerHTML = `A New Day Begins...<br>Time: ${timeNumber}`;
   }
+
+  updateGame();
 }
 
 const addWood = (event) => {
   event.preventDefault();
 
-  let timeToWood = 4;
+  let timeToWood = isAxeMade ? 2 : 5;
   woodCounter.innerHTML = `Wood: ${woodNumber++}`;
 
   updateTime(timeToWood);
@@ -40,7 +67,29 @@ const addStone = (event) => {
   updateTime(timeToStone);
 }
 
-setupGame();
+const makeAxe = (event) => {
+  event.preventDefault();
 
-chopBtn.addEventListener("click", addWood);
-gatherBtn.addEventListener("click", addStone);
+  if (woodNumber >= 8 && stoneNumber >= 4) {
+    woodNumber -= 8;
+    stoneNumber -= 4;
+    isAxeMade = true;
+    actionList.removeChild(actionList.lastElementChild);
+
+    newMessage.innerHTML = "Should take less time to get wood now.";
+    messageCenter.appendChild(newMessage);
+
+  } else if (woodNumber < 8 || stoneNumber < 4) {
+    newMessage.innerHTML = `You don't have enough ${woodNumber < 8 ? "Wood" : "Stone"}`;
+    messageCenter.appendChild(newMessage);
+
+  } else {
+    newMessage.innerHTML = `You don't have enough materials`;
+    messageCenter.appendChild(newMessage);
+  }
+}
+
+updateGame();
+
+woodBtn.addEventListener("click", addWood);
+stoneBtn.addEventListener("click", addStone);
